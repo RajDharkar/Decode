0d5bf42c8eebc76b02b34041d17b79bceef183f6
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-10-25 09:53:55 -0400

    constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/RollUpConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/RollUpConstants.java
@@ -1,10 +1,6 @@
 package org.firstinspires.ftc.teamcode.utils.constants;
 
-import com.acmerobotics.dashboard.config.Config;
-
-@Config
-public class IntakeConstants {
+public class RollUpConstants {
     public static double forwardPower = 1.0;
-    public static double backwardPower = -1.0;
     public static double offPower = 0.0;
 }
