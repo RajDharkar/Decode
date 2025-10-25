ef6b0063e46ed21280e5e9067eda5c6bfa552ca7
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-10-25 09:25:34 -0400

    Intake Constants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/IntakeConstants.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.utils.constants;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class IntakeConstants {
+    public static double forwardPower = 1.0;
+    public static double backwardPower = -1.0;
+
+    public static double offPower = 0.0;
+}
