adadec69475482112cd935eb6b0de76f97390d16
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-10-31 16:57:42 -0400

    Update ShooterCalculations.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/calculations/ShooterCalculations.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/calculations/ShooterCalculations.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.robot.calculations;
 import java.util.ArrayList;
 
 public class ShooterCalculations {
+
     public static ShooterInput getHoodAndPower(){
         return new ShooterInput(0, 0);
     }
