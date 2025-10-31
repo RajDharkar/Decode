3c65e904a67b9ca52fa561170249610096dba94c
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-10-31 16:57:44 -0400

    Update ShooterInput.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/calculations/ShooterInput.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/calculations/ShooterInput.java
@@ -1,25 +1,24 @@
 package org.firstinspires.ftc.teamcode.robot.calculations;
 
 public class ShooterInput {
-    private double hoodAngle;
-    private double motorPower;
-
-    public ShooterInput(double hoodAngle, double motorPower) {
+    private double hoodAngle, motorRPM;
+    public ShooterInput(double hoodAngle, double motorRPM) {
         this.hoodAngle = hoodAngle;
-        this.motorPower = motorPower;
+        this.motorRPM = motorRPM;
         //hoodAngle is the servo position, between 0 and 1
+        //motorRPM is just the rpm
     }
 
     public void setHoodAngle(double hoodAngle){
         this.hoodAngle = hoodAngle;
     }
-    public void setMotorPower(double motorPower){
-        this.motorPower = motorPower;
+    public void setMotorRPM(double motorRPM){
+        this.motorRPM = motorRPM;
     }
-    public double getHoodAngle(double motorPower){
-        return motorPower;
+    public double getHoodAngle(){
+        return hoodAngle;
     }
-    public double hoodAngle(){
-        return motorPower;
+    public double getMotorRPM(){
+        return motorRPM;
     }
 }
