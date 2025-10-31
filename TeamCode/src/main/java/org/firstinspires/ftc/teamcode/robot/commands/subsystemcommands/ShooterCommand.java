015a0f979ca723ff7e5ea40a066b258082dd0c14
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-10-31 16:57:54 -0400

    Update ShooterCommand.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/subsystemcommands/ShooterCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/subsystemcommands/ShooterCommand.java
@@ -1,4 +1,18 @@
 package org.firstinspires.ftc.teamcode.robot.commands.subsystemcommands;
 
-public class ShooterCommand {
+import com.arcrobotics.ftclib.command.CommandGroupBase;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+
+import org.firstinspires.ftc.teamcode.robot.Robot;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Shooter;
+
+public class ShooterCommand extends SequentialCommandGroup {
+    public ShooterCommand(Robot robot, Shooter.ShooterState state){
+        addCommands(
+                new InstantCommand(() -> robot.shooter.setState(state)),
+                new WaitCommand(500)
+        );
+    }
 }
