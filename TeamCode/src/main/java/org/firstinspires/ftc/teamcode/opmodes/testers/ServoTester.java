7540b3b3b3e1523ba6c08801b9fa22ae0cb06bed
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-11-04 17:11:47 -0500

    Create ServoTester.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testers/ServoTester.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.opmodes.testers;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.utils.MyTelem;
+
+@Config
+@TeleOp(name = "Servo Tester")
+public class ServoTester extends LinearOpMode {
+    public static String hardwareMapName = "servo";
+    public static double servoPos = 0.0;
+
+    public void runOpMode() throws InterruptedException{
+        Servo servo = hardwareMap.get(Servo.class, hardwareMapName);
+        double currentServoPos = servoPos;
+
+        while (opModeIsActive()){
+            if (servoPos != currentServoPos){
+                servo.setPosition(servoPos);
+            }
+            currentServoPos = servoPos;
+        }
+    }
+
+}
