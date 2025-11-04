130c1687834378719f93a0a0ccffe5298df76f83
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-11-04 17:12:10 -0500

    turret stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/TurretConstants.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.utils.constants;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class TurretConstants {
+    public static double turretForwardPosition = 0.0;
+}
