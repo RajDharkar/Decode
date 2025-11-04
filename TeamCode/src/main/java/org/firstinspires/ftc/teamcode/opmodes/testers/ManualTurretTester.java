c9b39f8e9df3947d5b84d6a4ef1c318156a86515
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-11-04 17:11:39 -0500

    turret

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testers/ManualTurretTester.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.opmodes.testers;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.robot.Robot;
+import org.firstinspires.ftc.teamcode.robot.commands.subsystemcommands.ShooterCommand;
+import org.firstinspires.ftc.teamcode.robot.commands.subsystemcommands.TurretCommand;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Shooter;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Turret;
+import org.firstinspires.ftc.teamcode.utils.MyTelem;
+
+@TeleOp(name = "Turret Tester and Tuner")
+public class ManualTurretTester extends LinearOpMode {
+    public void runOpMode(){
+        MyTelem.init(telemetry);
+        Robot robot = new Robot(hardwareMap, false);
+
+        GamepadEx gp1 = new GamepadEx(gamepad1);
+        gp1.getGamepadButton(GamepadKeys.Button.A).whenPressed(new TurretCommand(robot, Turret.TurretState.FRONT));
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            robot.update();
+        }
+        robot.stop();
+    }
+
+}
