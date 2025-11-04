d6fb8cc6a53833d36c19724bb7f30964fb50b8fe
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-11-04 17:12:04 -0500

    Create Turret.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Turret.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems;
+import com.arcrobotics.ftclib.command.Subsystem;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.utils.constants.IntakeConstants;
+import org.firstinspires.ftc.teamcode.utils.constants.TurretConstants;
+
+public class Turret implements Subsystem {
+    public Servo turretLeftServo, turretRightServo;
+    public TurretState state;
+    public Turret(Servo turretLeftServo, Servo turretRightServo){
+        this.turretLeftServo = turretLeftServo;
+        this.turretRightServo = turretRightServo;
+
+        state = TurretState.FRONT;
+    }
+    public void setState(TurretState state){
+        this.state = state;
+        switch (state){
+            case FRONT:
+                turretLeftServo.setPosition(TurretConstants.turretForwardPosition);
+                turretRightServo.setPosition(1 - TurretConstants.turretForwardPosition);
+                break;
+            case MATH:
+                break;
+        }
+    }
+
+    public TurretState getState(){
+        return state;
+    }
+    public enum TurretState{
+        FRONT, MATH
+    }
+
+}
