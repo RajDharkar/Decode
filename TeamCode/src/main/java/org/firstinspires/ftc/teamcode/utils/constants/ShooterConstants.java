c2d80844964e99480a739aa4949614ec2bb03a53
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-11-05 23:56:38 -0500

    constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/ShooterConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/ShooterConstants.java
@@ -4,9 +4,9 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class ShooterConstants {
-    public static double closeShootRPM = 1850; // 1800
-    public static double farShootRPM = 3000; //2.16k
-    public static double kf = 0.1, kp = 0.02, ki = 0, kd = 0.000001;
+    public static double closeShootRPM = 1740; // 1800
+    public static double farShootRPM = 2150; //2.16k
+    public static double kf = 0.0, kp = 0.15, ki = 0, kd = 0.000001;
     public static double tuningTestingRPM = 0;
     public static int TICKS_PER_REV = 28;
     public static int MAX_RPM = 6000;
