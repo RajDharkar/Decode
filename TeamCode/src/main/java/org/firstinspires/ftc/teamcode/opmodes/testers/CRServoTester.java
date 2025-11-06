a3681e118eef253d896666d3a8303c24b3267e24
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-11-05 23:56:31 -0500

    testers

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testers/CRServoTester.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.opmodes.testers;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+@Config
+@TeleOp(name = "CR Servo Tester")
+public class CRServoTester extends LinearOpMode {
+    public static String servoName = "";
+    public static double power = 0.1;
+
+    public void runOpMode(){
+        CRServo servo = hardwareMap.get(CRServo.class, servoName);
+        while(opModeIsActive()){
+            servo.setPower(power);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testers/TwoServoTester.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.opmodes.testers;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+@TeleOp(name = "Two Servo Tester")
+public class TwoServoTester extends LinearOpMode {
+    public static String regServo = "";
+    public static String revServo = "";
+    public static double position = 0.1;
+    public void runOpMode(){
+
+        Servo a = hardwareMap.get(Servo.class, regServo);
+        Servo b = hardwareMap.get(Servo.class, revServo);
+        b.setDirection(Servo.Direction.REVERSE);
+        while(opModeIsActive()){
+            a.setPosition(position);
+            b.setPosition(position);
+        }
+    }
+}
